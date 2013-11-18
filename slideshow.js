252575c767030a6ba6507194784733eb6ceb55a1
Serubin <serubin@serubin.net>
2013-11-18 10:44:16 -0500

    slideshow.js is slideshow version 2 not complete.

--- slideshow.js
+++ slideshow.js
@@ -1,319 +1,322 @@
 /* 
- * Serubin.net - Photo SlideShow
- * Author: Solomon Rubin
- * Version: 1.0
+ * Photo SlideShow
+ * Author: Solomon Rubin, Serubin.net
+ * Version: 2.0
  * 
  * Do not distribute.
  */
-//TODO add custom css. Custom image size. Custom interval. Custom HTML? Add other transitions!
  
-//User set variables
-var SlideShow = function(){
-	var ssEl;
-	var loadOnStart = true;
-	var pre_load = false; 
-	var random = false;
-	var defaultInterval = 10000;
-	var hoverPause = true;
-	var preview = true;
-	var transition = "normal";
+function SlideShow(){
+	/* Functions Vars */
+	
+	var addSlide;
+	
+	/* User Vars (Defaults); */
+	var startOnLoad = true; // Start once SlideShow has loaded
+	var pre_load = false; // Pre-load images
+	var random = false; // Progress through slides randomly
+	var defaultInterval = 10000; // Default in MS (10 seconds);
+	var hoverPuase = true; // Pause when hovered over image
+	var transition = "normal"; // TODO create different transitions
+	var hoverPreview = true; // Image preview on selector dots
+	var hoverText = "Click for more!"; // Text displayed when hovered over slide
+	var height = 550; // Default frame size
+	var width = 925; // Default frame size
+	var debugM = false; // debug messages are false by default
 	var onload;
-
-	var initiated = false;
-	var IE = false;
-	var imageNum = 0;
-	var images = new Array();
-	var hovers = new Array();
-	var total;
-	var active = 0;
-	var fadeCBT = 500;
-	var width = 925;
-	var height = 550;
-
-	//Check IE
-	if(navigator.appName.indexOf("Internet Explorer")!=-1){
-		IE = true;
+	
+	/* Script Vars */
+	var slides = new Array();
+	var selectors = new Array();
+	var slidesLoaded = "";
+	var curSlide = 0; // Current Slide;
+	var curTimer;
+	
+	/* Element Vars */
+	var slideShowEl; // SlideShow Element Object
+	var linkEl;
+	var imageEl;
+	var completeEl;
+	var titleEl;
+	var descEl;
+	var selectEl;
+	
+	/* Private Functions */
+	function initiate(arguments){
+		// Collects init data from anonymous object
+		if(typeof arguments[0].element != "undefined"){
+			slideShowEl = document.getElementById(arguments[0].element);
+		} else {
+			slideShowEl = document.getElementById("slideshow");
+		}
+		if(typeof arguments[0].pre_load != "undefined"){
+			pre_load = arguments[0].pre_load;
+		}
+		if(typeof arguments[0].random != "undefined"){
+			random = arguments[0].random;
+		}
+		if(typeof arguments[0].interval != "undefined"){
+			defaultInterval = arguments[0].interval;
+		}
+		if(typeof arguments[0].hover_pause != "undefined"){
+			hoverPause = arguments[0].hover_pause;
+		}
+		if(typeof arguments[0].transition != "undefined"){
+			transition = arguments[0].transition;
+		}
+		if(typeof arguments[0].hover_preview != "undefined"){
+			hoverPreview = arguments[0].hover_preview;
+		}
+		if(typeof arguments[0].hover_text != "undefined"){
+			hoverText = arguments[0].hover_text;
+		}
+		if(typeof arguments[0].height != "undefined"){
+			height = arguments[0].height;
+		}
+		if(typeof arguments[0].width != "undefined"){
+			width = arguments[0].width;
+		}
+		if(typeof arguments[0].startOnLoad != "undefined"){
+			startOnLoad = arguments[0].startOnLoad;
+		}
+		if(typeof arguments[0].debug != "undefined"){
+			debugM = arguments[0].debug;
+		}
+		if(typeof arguments[0].onload != "undefined"){
+			onload = arguments[0].onload;
+		}
+		debug("init");
 	}
-	//End Check IE
-
-	// Timer variables
-	var interval;
-	var timerID;
-	var startTimeMS;
-	var paused = false;
-	/*
-	 * Data Arrays, Processing, and Setup
-	 */
-	var dataLoad = false;
-	return{
-		pushData:function(){
-			for(var i = 0;arguments.length > i;i++){
-				images.push(new image(i, arguments[i].url, arguments[i].title, arguments[i].link, arguments[i].desc));
+	
+	function create(){
+		// Error checking for empty slides
+		if(slides.length < 1){
+			error("function create: no slides loaded, cannot create slideshow DOM objects");
+			return 0;
+		}
+		
+		slideShowEl.className = "slide-container";
+		linkEl = document.createElement("a");
+			linkEl.id = "slide-link";
+			linkEl.title = hoverText;
+		imageEl = document.createElement("img");
+			imageEl.id = "slide-image";
+			imageEl.width = width;
+			imageEl.height = height;
+		linkEl.appendChild(imageEl);
+		
+		// Creates "meta" bar
+		var bar = document.createElement("div");
+			bar.id = "slide-bar";
+		completeEl = document.createElement("div");
+			completeEl.id = "slide-complete";
+		bar.appendChild(completeEl);
+		
+		titleEl = document.createElement("div");
+			titleEl.id = "slide-title";
+		bar.appendChild(titleEl);
+		
+		descEl = document.createElement("div");
+			descEl.id = "slide-desc";
+		bar.appendChild(descEl);
+		
+		// Creates selectors
+		var select = document.createElement("ul");
+			select.id = "select";
+		
+		for(var i = 0;total > i;i++){
+			var li = document.createElement("li");
+				li.className = "slide-li-photo-" + i;
+			var a = document.createElement("a");
+				a.href = "javascript:void(0);";
+				a.className = "photo-button"
+				a.id = "slide-photo-" + i;
+				a.title = slides[i].title;
+				a.addEventListener("click", function(){
+					switchImage(this.id.parseInt());
+				});
+			li.appendChild(a);
+			selectors.push({"id":i,"element":li});
+			// Hover preview stuff
+			if(hoverPreview){
+				//TODO add hover preview stuff
+				var span = document.createElement("span");
+					span.className = "select-hover";
+					span.style.display = "none";
+				var img = document.createElement("img");
+					img.src = slides[i].img;
+					img.width = "200";
+					img.height = "100";
+				span.appendChild(img);
+				li.appendChild(span);
+				
+				// Event listeners for mouse in and out
+				span.addEventListener("mouseover", function(){
+					this.style.display = "";
+				});
+				span.addEventListener("mouseout", function(){
+					this.style.display = "none";
+				});
 			}
-			total = images.length;
-			dataLoad = true;
-		},
-		init:function(){
-			if(typeof arguments[0].element != "undefined"){
-				ssEl = document.getElementById(arguments[0].element);
-			} else {
-				ssEl = document.getElementById("slideshow");
-			}
-			if(typeof arguments[0].pre_load != "undefined"){
-				pre_load = arguments[0].pre_load;
-			}
-			if(typeof arguments[0].random != "undefined"){
-				random = arguments[0].random;
-			}
-			if(typeof arguments[0].interval != "undefined"){
-				defaultInterval = arguments[0].interval;
-			}
-			if(typeof arguments[0].hover_pause != "undefined"){
-				hoverPause = arguments[0].hover_pause;
-			}
-			if(typeof arguments[0].transition != "undefined"){
-				transition = arguments[0].transition;
-			}
-			if(typeof arguments[0].preview != "undefined"){
-				preview = arguments[0].preview;
-				console.log(arguments[0].preview);
-			}
-			if(typeof arguments[0].height != "undefined"){
-				height = arguments[0].height;
-			}
-			if(typeof arguments[0].width != "undefined"){
-				width = arguments[0].width;
-			}
-			if(typeof arguments[0].onload != "undefined"){
-				onload = arguments[0].onload;
-			}
-			if(dataLoad){
-				initiated = true;
-				loadData();
-				start();
-			}
-		},
-		image:function(i){
-			clearTimer();
-			selectImage(i);
-		},
-		next:function(){
-			switchImage(1)
-		},
-		prev:function(){
-			switchImage(-1);
-		},
-		rand:function(){
-			switchImage(1, images[(Math.floor(Math.random()*total))]);
-		},
-		pause:function(){
-			pauseTimer();
-		},
-		resume:function(){
-			resumeTimer();
-		},
-		isPaused:function(){
-			return paused;
+		}
+		
+		slideShowEl.appendChild(linkEl);
+		slideShowEl.appendChild(bar);
+		if(startOnLoad){
+			this.start();
 		}
 	}
 	
-	function loadData(){
-		total = images.length;
-		imageNum = (total - 1);
-		ssEl.innerHTML = "<a id='slide-link' title='Click for album!' href=''><img id='slideImg' src='" + images[0].url + "' width='" + width + "px' height='" + height + "px'/></a><div id='meta'><div id='complete' style='width:900px;'></div><div id='photo-title'></div><div id='photo-desc'></div><ul id='select'></ul></div>";
-		displaySelect();
-		return true;
-	}
-
-	// Pre-load images
 	function preLoadImages(){
-		
-		var el = document.createElement("div");
-		el.setAttribute("id", "preLoad");
-		ssEl.appendChild(el);
-		el = document.getElementById("preLoad");
-		var img = "";
 		for(var i = 0;total > i;i++){
-			img += "<img src='"+images[i].url+"' style='display:none;'/> "
-		}
-		el.innerHTML = img;
-	}
-
-	// Creates button for display
-	function displaySelect(){
-		var string = "";
-		for(var i = 0;total > i;i++){
-			// Create dot selectors
-			string += "<li id='li-photo-"+i+"'><a href='#"+i+"' title='"+images[i].title+"' class='photo-button' id='photo-"+i+"'onClick='javascript:SlideShow.image("+i+");'></a><span class='select-hover' style='display:none;'><img src='"+images[i].url+"' width='200' height='100'/></span></li>     ";
-			hovers.push("li-photo-"+i+":"+i);
-		}
-		document.getElementById("select").innerHTML = string;
-	}
-	function selectHover(object, image){
-		console.log("Creating new hover event");
-		object.onmousemove = function(){openHover()};
-		object.onmouseout = function(){closeHover()};
-		function openHover(){
-			console.log("Open hover called");
-			object.getElementsByTagName("span")[0].style.display = "";
-		}
-		function closeHover(){
-			console.log("Close hover called");
-			object.getElementsByTagName("span")[0].style.display = "none";
+			var preLoadImg = new Image();
+			preLoadImg.onload = function(){
+				debug("Image " + i + "was preloaded.");
+			}
+			preLoadImg.src = slides[i].img;
 		}
 	}
-	function checkIE(){
-		if(!IE){
-			document.getElementById("slideImg").className = "transition";
+	
+	function nextImage(){
+		if(!random){
+			switchImage((imageNum+1) % total);
 		} else {
-			fadeCBT = 1;
+			switchImage(Math.floor(Math.random()*total));
 		}
 	}
-	/*
-	* End Data Arrays, Processing and Setup
-	*/
- 
-	function image(id, url, title, link, desc){
+	
+	function prevImage(){
+	
+	}
+	
+	// Switches image based on ID
+	function switchImage(id){
+		if(id > slides.length){
+			error("No slide found");
+		}
+		var next = slides[id];
+		curSlide = id;
+		
+	}
+	
+	// Switches text based on ID
+	function switchText(id){
+		var next = slides[id];
+		linkEl.href = next.link;
+		titleEl.innerHTML = next.title;
+		descEl.innerHMLT = next.desc;
+	}
+	
+	// Holds different transitions
+	function animate(mode, id, callback){
+		switch(mode){
+			case "fade":
+			imageEl.className += " transitionFade";
+			imageEl.style.opacity = 0;
+			setTimeout(function(){
+				imageEl.src = slides[id].img;
+				imageEl.style.opacity = 100;
+				
+				curTimer.stop();
+				curTimer = new Timer(nextImage(), defaultInterval);
+				imageEl.className.replace(" transitionFade", "");
+			}, 500);
+			break;
+			case "slide":
+			
+			break;
+			default:
+				error("No transition found");
+			break;
+		}
+		callback();
+	}
+	
+	// Sets new and old selectors to active and non active respectively
+	function setActiveSelector(newId, curId){
+		selectors[newId].element.firstChild.className += "photo-button-active";
+		selectors[curId].element.firstChild.className = "photo-button";
+	}
+	
+	function error(text){
+		throw "SlideShow: " + text;
+	}
+	
+	function debug(text){
+		if(debugM){
+			console.log("DEBUG SlideShow: "+ text);
+		}
+	}
+	
+	/* Public Functions */
+	
+	this.addSlide = function(){
+		if(arguments.length < 0){
+			error("No slides to add");
+			return 0;
+		}
+		for(var i = 0;arguments.length > i;i++){
+			slides.push(new Slide(i, arguments[i].img, arguments[i].title, arguments[i].link, arguments[i].desc));
+		}
+		total = slides.length;
+		create();
+	}
+	
+	this.start = function(){
+		onload();
+		nextImage();
+	}
+	
+	/* Internal Objects */
+	
+	// Holds Slide Data
+	function Slide(id, img, title, link, desc){
 		this.id = id
-		this.url = url;
+		this.img = img;
 		this.title = title;
 		this.link = link;
 		this.desc = desc;
 	}
-
-	// Gets next image
-	function getNextImage(){
-		if(!random){
-			imageNum = (imageNum+1) % total;
-		} else {
-			imageNum = Math.floor(Math.random()*total);
+	
+	function Timer(call, interval){
+		/* Timer Public Function Vars */
+		var start;
+		var stop;
+		var pause;
+		var getTime;
+		
+		/* Timer Vals */
+		var startTime;
+		var timerId;
+		
+		// Starts timer		
+		function startTimer(){
+			startTime = (new Date()).getTime();
+			timerID = setTimeout(call, interval);
 		}
-		var new_image = images[imageNum];
-		return new_image;
-	}
-
-	// Get prev image
-	function getPrevImage(){
-		if(imageNum == 0){
-			imageNum = (total-1);
-		} else {
-			imageNum = (imageNum-1) % total;
+		
+		// Stops timer
+		function stopTimer(){
+			interval = this.getTime();
+			clearTimeout(timerId);
 		}
-		var new_image = images[imageNum];
-		return new_image;
-	}
-
-	function selectImage(image){
-		imageNum = image;
-		return switchImage(1, images[image]);
-	}
-
-	// Switches image on screen (with fade)
-	function switchImage(dir){
-		if(!dir){
-			dir = 1;
+		
+		/* Public Timer Functions */
+		this.start = function(){
+			startTimer();
 		}
-		var object = document.getElementById("slideImg");
-		var next;
-		if(arguments.length == 2){
-			next = arguments[1];
-		} else {
-			if(dir < 1){
-				next = getPrevImage();
-			} else {
-				next = getNextImage();
-			}
+		
+		this.stop = function(){
+			stopTimer();
 		}
-		console.log("Loading image " + next.title);
-		object.style.opacity = 0; 
-		setTimeout(function(){	
-			object.src = next.url;
-			document.getElementById("photo-title").innerHTML = next.title;
-			document.getElementById("photo-desc").innerHTML = next.desc;
-			document.getElementById("slide-link").href = next.link;
-			object.style.opacity = 100;
-			interval = defaultInterval
-			var recur_call = switchImage;
-			clearTimer();
-			startTimer(recur_call, interval);
-			setActive(next.id);
-		}, fadeCBT);
-	}
-
-	function setActive(id){
-		document.getElementById("photo-"+active).className = "photo-button";
-		document.getElementById("photo-"+id).className += " photo-button-active";
-		active = id;
-	}
-   /*
-	* TIMER FUNCTIONS
-	*/
-	//TODO rewrite to use both timer ids and to be a variable function - object thingy
-	// Starts timer
-	function startTimer(recur_call, interval){
-		startTimeMS = (new Date()).getTime();
-		timerID = setTimeout(recur_call, interval);
-	}
-	// Pauses timer
-	function pauseTimer(){
-		if(!paused){
-			interval = getRemainingTime();
-			clearTimer();
-			paused = true;
+		
+		this.getTime = function(){
+			return(interval-((new Date()).getTime()-startTime));
 		}
+		
+		// Starts
+		startTimer();
 	}
-
-	// Resumes timer from pause location
-	function resumeTimer(recur_call){
-			startTimer(recur_call, interval);
-			paused = false;
-	}
-	// Clears timer
-	function clearTimer(){
-		clearTimeout(timerID);
-	}
-
-	// Get remaining time on defualt timer
-	function getRemainingTime(){
-		return(interval-((new Date()).getTime()-startTimeMS));
-	}
-
-	// Displays time in a visual format
-	function displayTime(){
-		var timeR = getRemainingTime();
-		if(!paused){
-			document.getElementById("complete").style.width = (timeR/(defaultInterval/900) + "px");
-		}
-		setTimeout(displayTime, 10);
-	}
-	/*
-	* SCRIPT START
-	*/
-	function start(){
-		checkIE();
-		if(!ssEl){
-			console.error("SlideShow element specified, not found!");
-		}
-		if(initiated){
-			if(pre_load){
-				preLoadImages();
-			}
-			console.log(preview);
-			if(preview){
-				for(var i = 0;total > i;i++){
-					var temp = hovers[i].split(":");
-					hovers.push(new selectHover(document.getElementById(temp[0]),temp[1]));
-				}
-			}
-			interval = defaultInterval;
-			switchImage(1);
-			displayTime();
-			console.log("Page loaded and setup!");
-			if(hoverPause){
-				document.getElementById("slideImg").onmousemove = function(){pauseTimer();};
-				document.getElementById("slideImg").onmouseout = function(){resumeTimer(switchImage);};
-			}
-			onload();
-		}
-	}
-}();
+	
+	initiate(arguments);
+}
